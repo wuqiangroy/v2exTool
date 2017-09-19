b281416b05bf85750c6f407c1c66d8cd0d95343f
wuqiangroy <wuqiangroy@live.com>
2017-09-20 00:32:26 +0800

    add config

---
+++ config.py
@@ -0,0 +1,13 @@
+"""
+this is v2ex config
+including headers, urls
+"""
+
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.91 Safari/537.36",
+    "referer": "https://www.v2ex.com/signin" 
+}
+main_url = "https://www.v2ex.com"
+login_url = "https://www.v2ex.com/signin"
+check_in_url = "https://www.v2ex.com/mission/daily"
+user_info_url = "https://www.v2ex.com/api/members/show.json"
--- v2extool.py
+++ v2extool.py
@@ -1,36 +1,40 @@
 import re
 import requests
+from config import headers, main_url, login_url, check_in_url, user_info_url
 
 
 class v2extool:
+    """main func"""
 
-    headers = {
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.91 Safari/537.36",
-        "referer": "https://www.v2ex.com/signin" 
-    }
     session = requests.session()
 
     def login(self, username, password):
-        login_url = "https://www.v2ex.com/signin"
         data = {"username": username, "password": password}
         res = self.session.post(url=login_url, data=data)
         print(res.text.encode().decode())
     
     @property
     def check_in(self):
-        check_in_url = "https://www.v2ex.com/mission/daily"
         res = self.session.get(check_in_url)
-        if "每日登录奖励已领取" in res.text.encode().decode():
+        if "每日登录奖励已领取" in res.text:
             print("error: you have been already checked in")
             return False
-        resp = self.session.get('http://v2ex.com' + re.search(r'/mission/daily/redeem\?once=\d+', res.text).group())
+        resp = self.session.get(main_url + re.search(r'/mission/daily/redeem\?once=\d+', res.text).group())
         if resp.ok:
             print("success: you just checked in success!")
             return True
     
-
+    def user_info(self, username=None, user_id=None):
+        if not (username or user_id):
+            raise TypeError("You must type one params: usernae or user_id")
+        user_url = user_info_url + "?username={}&id={}".format(username, user_id)
+        res = requests.get(user_url)
+        print(res.json())
+        return True
 
 if __name__ == "__main__":
     v = v2extool()
     v.login(username="wuqiangroy", password="123321")
+    # v.check_in
+    # v.user_info("wuqiangroy")
 
