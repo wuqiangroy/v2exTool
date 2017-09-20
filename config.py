ad9947628771ed2ea0ad1d3a87d881f06cd2a82b
wuqiang <wuqiang@wolongdata.com>
2017-09-20 18:52:06 +0800

    ^_^

--- config.py
+++ config.py
@@ -4,9 +4,12 @@ including headers, urls
 """
 
 headers = {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.91 Safari/537.36",
-    "referer": "https://www.v2ex.com/signin" 
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:49.0) Gecko/20100101 Firefox/49.0",
+    "Referer": "http://www.v2ex.com/signin",
+    "Origin": "http://www.v2ex.com",
+    "Host": "www.v2ex.com"
 }
+
 main_url = "https://www.v2ex.com"
 login_url = "https://www.v2ex.com/signin"
 check_in_url = "https://www.v2ex.com/mission/daily"
--- v2extool.py
+++ v2extool.py
@@ -1,5 +1,7 @@
 import re
+import json
 import requests
+from bs4 import BeautifulSoup
 from config import headers, main_url, login_url, check_in_url, user_info_url
 
 
@@ -7,19 +9,39 @@ class v2extool:
     """main func"""
 
     session = requests.session()
+    proxy = {"https": "http://61.160.208.222:8080"}
 
     def login(self, username, password):
-        data = {"username": username, "password": password}
-        res = self.session.post(url=login_url, data=data)
-        print(res.text.encode().decode())
+
+        html = self.session.get(login_url, proxies=self.proxy, timeout=10).text
+        if "Access Denied" in html:
+            print("Your access denied by v2ex, please change your proxy")
+            return False
+        soup = BeautifulSoup(html, "html.parser")
+        param = soup.find_all("input")
+        data = {
+            param[1]["name"]: username,
+            param[2]["name"]: password,
+            param[3]["name"]: param[3]["value"],
+            param[5]["name"]: param[5]["value"]
+        }
+        res = self.session.post(url=login_url, params=data, headers=headers, proxies=self.proxy, timeout=10)
+        if "登录有点问题，请重试一次" in res.text:
+            print("login failed, please check your username or password")
+            return False
+        else:
+            print("login success")
+            return True
     
     @property
     def check_in(self):
-        res = self.session.get(check_in_url)
+        res = self.session.get(check_in_url, proxies=self.proxy, timeout=10)
+
         if "每日登录奖励已领取" in res.text:
             print("error: you have been already checked in")
             return False
-        resp = self.session.get(main_url + re.search(r'/mission/daily/redeem\?once=\d+', res.text).group())
+        daily_redeem = main_url + re.search(r'/mission/daily/redeem\?once=\d+', res.text).group()
+        resp = self.session.get(daily_redeem, proxies=self.proxy, timeout=10)
         if resp.ok:
             print("success: you just checked in success!")
             return True
@@ -28,13 +50,70 @@ class v2extool:
         if not (username or user_id):
             raise TypeError("You must type one params: usernae or user_id")
         user_url = user_info_url + "?username={}&id={}".format(username, user_id)
-        res = requests.get(user_url)
+        res = requests.get(user_url, proxies=self.proxy, timeout=10)
         print(res.json())
         return True
 
+    def node_content(self, node_name="tech"):
+        content = []
+        res = self.session.get(main_url + "/?tab={}".format(node_name), proxies=self.proxy, timeout=10)
+        soup = BeautifulSoup(res.text, "html.parser")
+        items = soup.find_all(attrs={"class": "cell item"})
+        for item in items:
+            data = item.find("span")
+            content.append({
+                "node_name": node_name,
+                "article_id": re.search(r"\d+", str(data)).group(),
+                "article_name": re.search(r"reply\d+", str(data)).group()[5:],
+                "reply_num": data.text,
+                "author": item.find("strong").text
+            })
+        print(content)
+        return content
+
+    def article_info(self, article_id):
+
+        try:
+            int(article_id)
+        except ValueError as e:
+            raise ValueError("article_id must a int or all numeric strings")
+        article_detail = {}
+        reply_content = []
+        res = self.session.get(main_url + "/t/{}".format(str(article_id)), proxies=self.proxy, timeout=10)
+        soup = BeautifulSoup(res.text, "html.parser")
+        data = soup.find(attrs={"class": "gray"}).text.split("·")
+        content = soup.find(attrs={"class": "markdown_body"}).text
+        author = data[0]
+        publish_time = data[1]
+        views = data[2]
+        title = soup.find("h1").text
+        article_detail.update({
+            "article_id": article_id,
+            "url": main_url + "/t/{}".format(str(article_id)),
+            "title": title,
+            "author": author,
+            "publish_time": publish_time,
+            "content": content,
+            "views": views,
+            "reply": reply_content
+        })
+        items = soup.find_all(attrs={"class": "cell"})
+        print(items)
+        for index in range(3, len(items)):
+            reply_content.append({
+                "reply_content": items[index].find(attrs={"class": "reply_content"}).text,
+                "reply_author": items[index].find("strong").text,
+                "create_time": items[index].find(attrs={"class": "ago"}).text,
+                "rank": items[index].find(attrs={"class": "no"}).text
+            })
+        article_detail.update({"reply_content": reply_content})
+        print(json.dumps(article_detail))
+
 if __name__ == "__main__":
     v = v2extool()
-    v.login(username="wuqiangroy", password="123321")
+    # v.login(username="wuqiangroy", password="123321")
     # v.check_in
     # v.user_info("wuqiangroy")
+    # v.node_content()
+    v.article_info(391903)
 
