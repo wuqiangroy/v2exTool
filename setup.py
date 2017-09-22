bafe2e9caf2300655e0027adb1cf4bbbe91f7c7b
wuqiang <wuqiang@wolongdata.com>
2017-09-22 12:09:53 +0800

    all

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='v2extool',
-      version='0.1',
-      description='a small tool for you to surf v2ex better',
-      author='Qiang Wu',
-      author_email='wuqiangroy@live.com',
-      url='https://github.com/wuqiangroy/v2exTool',
-      packages=['v2extool'],
-      license='MIT',
-      platforms='any',
-      classifiers=[],
-      )
+setup(
+    name='v2extool',
+    version='0.1',
+    description='a small tool for you to surf v2ex better',
+    author='Qiang Wu',
+    author_email='wuqiangroy@live.com',
+    url='https://github.com/wuqiangroy/v2exTool',
+    packages=['v2extool'],
+    license='MIT',
+    platforms='any',
+    )
